11521841690f4ac0629de71a1eec2c8166e800df
jeese <jeese@live.cn>
2015-02-28 16:45:16 +0800

    2015/02/28_01
    注册登录部分基本页面完成

--- helpme/gen/in/srain/cube/views/ptr/R.java
+++ helpme/gen/in/srain/cube/views/ptr/R.java
@@ -19,14 +19,14 @@ public final class R {
 		public static final int ptr_rotate_ani_time = 0x7f010008;
 	}
 	public static final class drawable {
-		public static final int ptr_rotate_arrow = 0x7f020088;
+		public static final int ptr_rotate_arrow = 0x7f02008e;
 	}
 	public static final class id {
-		public static final int ptr_classic_header_rotate_view = 0x7f0a00d1;
-		public static final int ptr_classic_header_rotate_view_header_last_update = 0x7f0a00d0;
-		public static final int ptr_classic_header_rotate_view_header_text = 0x7f0a00ce;
-		public static final int ptr_classic_header_rotate_view_header_title = 0x7f0a00cf;
-		public static final int ptr_classic_header_rotate_view_progressbar = 0x7f0a00d2;
+		public static final int ptr_classic_header_rotate_view = 0x7f0a00d2;
+		public static final int ptr_classic_header_rotate_view_header_last_update = 0x7f0a00d1;
+		public static final int ptr_classic_header_rotate_view_header_text = 0x7f0a00cf;
+		public static final int ptr_classic_header_rotate_view_header_title = 0x7f0a00d0;
+		public static final int ptr_classic_header_rotate_view_progressbar = 0x7f0a00d3;
 	}
 	public static final class layout {
 		public static final int cube_ptr_classic_default_header = 0x7f03002c;
--- helpme/src/jeese/helpme/activity/Splash.java
+++ helpme/src/jeese/helpme/activity/Splash.java
@@ -2,62 +2,46 @@ package jeese.helpme.activity;
 
 import jeese.helpme.R;
 import jeese.helpme.application.App;
+import jeese.helpme.auth.StartActivity;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.os.Handler;
 
 public class Splash extends Activity {
-
-	private final int SPLASH_DISPLAY_LENGHT = 0; // 延迟时间
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.splash);
 
-		// 判断app状态
+		// 获取登录状态数据
 		SharedPreferences preferences = getSharedPreferences("e_help",
 				Context.MODE_PRIVATE);
-		Boolean first_open = preferences.getBoolean("first_open_0.5.0", true);
-		Boolean login_status = preferences.getBoolean("login_status", true);
+		Boolean login_status = preferences.getBoolean("login_status", false);
 
-		// 是否第一次打开
-		if (first_open) {
-			// 当前版本应用第一次打开
-			// TODO 跳转欢迎页面
-		} else {
-			// 非第一次打开
-		}
-
-		// 登录状态
+		// 判断登录状态
 		if (login_status) {
 			// 已登录状态
 
+			//执行登陆
 			App myApp = ((App) getApplicationContext());
 			myApp.login();
 
-			new Handler().postDelayed(new Runnable() {
-
-				@Override
-				public void run() {
-
-					Intent mainIntent = new Intent(Splash.this,
-							MainActivity.class);
-					Splash.this.startActivity(mainIntent);
-					Splash.this.finish();
-
-				}
-
-			}, SPLASH_DISPLAY_LENGHT);
+			//页面跳转到主页面
+			Intent mainIntent = new Intent(Splash.this, MainActivity.class);
+			Splash.this.startActivity(mainIntent);
+			Splash.this.finish();
 
 		} else {
 			// 未登录状态
-			// TODO 跳转输入手机号页面
+		
+			//页面跳转到开始页面
+			Intent startIntent = new Intent(Splash.this, StartActivity.class);
+			Splash.this.startActivity(startIntent);
+			Splash.this.finish();
+			
 		}
 
 	}
